4766fac96723730bb77942d1c27cb1a963089103
Xiang An <anxiangsir@outlook.com>
2025-10-12 23:54:24 +0800

    build(packaging): add minimal setup.py shim
    Removed unnecessary comments and copyright notice.

--- setup.py
+++ setup.py
@@ -1,13 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: UTF-8 -*-
-################################################################################
-#
-# Copyright (c) 2024 Baidu.com, Inc. All Rights Reserved
-#
-################################################################################
-"""Setup script"""
-
 import setuptools
 
 setuptools.setup()
-
