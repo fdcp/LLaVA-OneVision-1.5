aeb40115260f0f43a344b82d4329bef500660f9b
fdcp <fdcp@163.com>
2025-10-31 14:01:25 +0800

    Modify the class to be compatible with packed MMR (Mixed & Multi-Round) data.

--- aiak_training_llm/data/multimodal/flavors/packed_captioning.py
+++ aiak_training_llm/data/multimodal/flavors/packed_captioning.py
@@ -9,6 +9,20 @@ import torch
 class PackedCaptioningSample(Sample):
     """Sample type for packed captioning."""
     # sample_id: str
-    images: List[torch.Tensor]
-    prompts: Optional[List[str]]
-    captions: List[str]
+    images: Union[
+        str,                    # A single image path, e.g., 'img001.jpg'
+        torch.Tensor,           # A single image tensor
+        List[str],              # A list of image paths, e.g., ['imgs001.jpg', 'imgs002.png']
+        List[List[str]],        # A nested list of image paths, e.g., [['imgs001.tif'], [], ['imgs005.png']]
+        List[torch.Tensor],     # A list of image tensors
+        List[List[torch.Tensor]] 
+    ]
+    prompts: Union[
+        Optional[List[str]],
+        List[List[str]]
+    ]
+    captions: Union[
+        List[str],
+        List[List[str]]
+    ]
+    
